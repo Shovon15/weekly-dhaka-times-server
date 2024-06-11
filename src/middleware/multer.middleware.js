62a7388bcfe03cb221f9347da33d5bfca0f6001e
shovon mahmud <shovonrnbd@gmail.com>
2024-06-11 15:57:42 +0600

    create dynamic image folder

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,8 +1,18 @@
 const multer = require('multer');
+const fs = require("fs");
+
+// Function to ensure directory exists, if not, create it
+const ensureDirectoryExistence = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+      const destinationFolder = 'public/book-images';
+      ensureDirectoryExistence(destinationFolder); // Ensure directory exists
       cb(null, 'public/book-images'); // Set the destination folder for the files
     },
     filename: function (req, file, cb) {
